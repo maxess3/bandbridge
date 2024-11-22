32c673a14afe948dc6208dff07810cbdb20f937d
maxess3 <maxschell31@gmail.com>
2024-11-22 11:59:34 +0100

    Add Prisma ORM

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const port = 5000;
-const app = express();
+import "dotenv/config";
+import express from "express";
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const port = process.env.PORT || 5000;
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
