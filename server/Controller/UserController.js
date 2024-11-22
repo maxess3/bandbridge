ddd89ca2f1a11f0e31b2e40ec07129111fa4fe64
maxess3 <maxschell31@gmail.com>
2024-11-22 15:09:50 +0100

    Add user controller and user route

---
+++ server/Controller/UserController.js
@@ -0,0 +1,27 @@
+import prisma from "../db/db.config.js";
+
+export const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (findUser) {
+    return res
+      .status(400)
+      .json({ message: "Email already exists, please use another one" });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password,
+    },
+  });
+
+  return res.status(200).json({ data: newUser, msg: "User created." });
+};
--- server/server.js
+++ server/server.js
@@ -4,9 +4,17 @@ import express from "express";
 const port = process.env.PORT || 5000;
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// Routes files
+import routes from "./routes/index.js";
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
