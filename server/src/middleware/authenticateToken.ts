e07d20a8872292fdb8ac5d2ebb71d9deb4be67f2
maxess3 <maxschell31@gmail.com>
2024-12-19 17:34:21 +0100

    Update auth cors

--- server/src/middleware/authenticateToken.ts
+++ server/src/middleware/authenticateToken.ts
@@ -5,7 +5,6 @@ const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   try {
-<<<<<<< HEAD
     if (!token) {
       res.sendStatus(401);
     } else {
@@ -15,19 +14,6 @@ const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     }
   } catch (error) {
     res.sendStatus(403);
-=======
-    const token = req.headers.authorization?.split(" ")[1];
-
-    if (!token) {
-      res.status(401).json({ msg: "Authentication required" });
-    }
-
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    (req as any).user = decodedToken;
-    next();
-  } catch (error) {
-    res.status(403).json({ msg: "Invalid token" });
->>>>>>> 7519692 (Add resfresh endpoint)
   }
 };
 
