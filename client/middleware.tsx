2acb0eae32afe7c2bbd0859a8843de7fe3935e0e
maxess3 <maxschell31@gmail.com>
2024-12-21 23:22:56 +0100

    update: middleware protected next routes

--- client/middleware.tsx
+++ client/middleware.tsx
@@ -2,14 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token");
+  const token = request.cookies.get("refreshToken");
   if (!token) {
-    return NextResponse.redirect(new URL(request.url).origin + "/login");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
   return NextResponse.next();
 }
 
 // Protected route
 export const config = {
-  matcher: [],
+  matcher: ["/me/:path*"],
 };
