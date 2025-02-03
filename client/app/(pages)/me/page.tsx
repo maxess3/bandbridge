569d152fdcfef5bead6d481f56e652191b1a5049
maxess3 <maxschell31@gmail.com>
2025-02-03 23:47:02 +0100

    update: profile

--- client/app/(pages)/me/page.tsx
+++ client/app/(pages)/me/page.tsx
@@ -4,31 +4,33 @@ import Image from "next/image";
 
 import { MdVerified } from "react-icons/md";
 
+import { FaYoutube } from "react-icons/fa";
+
+import { FaInstagram } from "react-icons/fa6";
+
+import { RiSoundcloudFill } from "react-icons/ri";
+
 import { DialogPopup } from "@/components/DialogPopup";
 
 export default function Me() {
   return (
-    <div className="space-y-3 mb-[400px]">
-      <div className="grid grid-cols-12 gap-3 h-full">
-        <div className="col-span-3 flex flex-col items-center relative">
-          <div className="w-full p-6 sticky top-0 bg-muted-background rounded-lg">
-            <div className="flex flex-col items-center space-x-3.5">
-              <div>
-                <span className="w-32 h-32 flex rounded-full overflow-hidden relative border-2 border-secondary">
-                  <Image
-                    width={300}
-                    height={300}
-                    src="/profile.jpeg"
-                    alt="Photo de profil"
-                    className="object-cover h-full shadow-xl"
-                  />
-                </span>
-              </div>
-              <div className="flex flex-col space-y-0.5 mt-1.5">
-                <span className="flex items-center text-2xl font-semibold">
-                  Maxime
-                  <MdVerified className="ml-1.5 text-[#2563eb]" />
-                </span>
+    <div className="mb-40 flex gap-3">
+      <div className="w-3/12 min-w-[280px] flex flex-col gap-3">
+        <div className="p-6 bg-muted-background">
+          <div className="flex flex-col justify-center text-center">
+            <div className="flex flex-col items-center">
+              <span className="w-36 h-36 flex rounded-full overflow-hidden relative border-2 border-secondary">
+                <Image
+                  width={300}
+                  height={300}
+                  src="/profile.jpeg"
+                  alt="Photo de profil"
+                  className="object-cover h-full shadow-xl"
+                />
+              </span>
+              <div className="flex flex-col mt-3">
+                <span className="text-2xl font-semibold">Maxime</span>
+                <span>@maxess</span>
               </div>
             </div>
             <div className="mt-4">
@@ -36,19 +38,41 @@ export default function Me() {
             </div>
           </div>
         </div>
-        <div className="col-span-9 bg-muted-background p-6 space-y-8 rounded-lg">
+        <div className="bg-muted-background p-6 space-y-3">
+          <h3 className="font-medium text-lg">Localisation</h3>
+          <span className="inline-block opacity-80">Non définie (31)</span>
+        </div>
+        <div className="bg-muted-background p-6 space-y-5">
+          <h3 className="font-medium text-lg">Liens</h3>
+          <ul className="space-y-3">
+            <li className="flex items-center">
+              <FaYoutube
+                className="mr-2.5"
+                style={{ width: "1.5em", height: "1.5em" }}
+              />
+              YouTube
+            </li>
+            <li className="flex items-center">
+              <RiSoundcloudFill
+                className="mr-2.5"
+                style={{ width: "1.5em", height: "1.5em" }}
+              />
+              SoundCloud
+            </li>
+            <li className="flex items-center">
+              <FaInstagram
+                className="mr-2.5"
+                style={{ width: "1.5em", height: "1.5em" }}
+              />
+              Instagram
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-9/12">
+        <div className="space-y-8 bg-muted-background p-6">
           <div className="space-y-3">
-            <div className="text-2xl">
-              <h2 className="font-semibold">Recherche</h2>
-            </div>
-            <div className="flex flex-wrap">
-              <ul>
-                <li>Chanteur</li>
-              </ul>
-            </div>
-          </div>
-          <div className="space-y-3">
-            <div className="text-2xl">
+            <div className="text-xl">
               <h2 className="font-semibold">Instruments</h2>
             </div>
             <div className="flex flex-wrap">
@@ -61,20 +85,18 @@ export default function Me() {
             </div>
           </div>
           <div className="space-y-3">
-            <div className="text-2xl">
-              <h2 className="font-semibold">Expérience</h2>
+            <div className="text-xl">
+              <h2 className="font-semibold">Expérience de l'instrument</h2>
             </div>
             <div className="flex flex-wrap">
-              <span className="text-sm border px-3 py-0.5 font-semibold rounded-full mr-2 mb-2">
-                Guitare
-              </span>
-              <span className="text-sm border px-3 py-0.5 font-semibold rounded-full mr-2 mb-2">
-                Piano
-              </span>
+              <ul>
+                <li>Guitare: 3ans</li>
+                <li>Guitare: 3ans</li>
+              </ul>
             </div>
           </div>
           <div className="space-y-3">
-            <div className="text-2xl">
+            <div className="text-xl">
               <h2 className="font-semibold">Styles</h2>
             </div>
             <div className="flex flex-wrap">
@@ -93,6 +115,17 @@ export default function Me() {
               <span className="text-sm border px-3 py-0.5 font-semibold rounded-full mr-2 mb-2">
                 Hip-Hop
               </span>
+            </div>
+          </div>
+          <div className="space-y-3">
+            <div className="text-xl">
+              <h2 className="font-semibold">Matériel</h2>
+            </div>
+            <div className="flex flex-wrap">
+              <ul>
+                <li>Guitare: 3ans</li>
+                <li>Guitare: 3ans</li>
+              </ul>
             </div>
           </div>
         </div>
