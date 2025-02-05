63a9202c68790e99bca762ddcca5706323dea378
maxess3 <maxschell31@gmail.com>
2025-02-05 22:01:11 +0100

    update: profile

--- client/app/(pages)/me/page.tsx
+++ client/app/(pages)/me/page.tsx
@@ -5,42 +5,87 @@ import Image from "next/image";
 import { GoVerified } from "react-icons/go";
 
 import { FaYoutube } from "react-icons/fa";
-
 import { FaInstagram } from "react-icons/fa6";
-
 import { RiSoundcloudFill } from "react-icons/ri";
 
 import { DialogPopup } from "@/components/DialogPopup";
 
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { IoMdTime } from "react-icons/io";
+import { AiOutlineUser } from "react-icons/ai";
+import { RiUserSearchLine } from "react-icons/ri";
+import { CiMusicNote1 } from "react-icons/ci";
+import { TbCrown } from "react-icons/tb";
+import { CgSearch } from "react-icons/cg";
+import { FaRegHandPaper } from "react-icons/fa";
+
 export default function Me() {
   return (
-    <div className="mb-40 flex flex-col">
-      <div className="flex items-center justify-between w-full h-44 px-6 bg-primary gap-4 relative">
-        <div className="w-40 h-40 flex rounded-full overflow-hidden border-4 border-muted-background absolute top-16">
-          <Image
-            width={300}
-            height={300}
-            src="/profile.jpeg"
-            alt="Photo de profil"
-            className="object-cover h-full shadow-xl"
-          />
-        </div>
-      </div>
-      <div className="space-y-3">
-        <div className="pt-16 pb-6 px-6 bg-muted-background">
-          <div className="flex flex-col">
-            <span className="text-2xl font-semibold inline-flex items-center">
-              Maxime <GoVerified className="ml-1.5" />
-            </span>
-            <span className="text-lg relative -top-1">@maxess</span>
-            <span className="text-base font-normal mt-2">
-              25 ans | Toulouse | Recherche un groupe
-            </span>
+    <div className="py-10 flex flex-col">
+      <div className="flex gap-x-8">
+        <div>
+          <div className="w-52 h-52 flex rounded-full overflow-hidden border border-secondary shadow-lg">
+            <Image
+              width={300}
+              height={300}
+              src="/profile.jpeg"
+              alt="Photo de profil"
+              className="object-cover h-full shadow-xl"
+            />
           </div>
         </div>
+        <div className="flex flex-col">
+          <span className="text-2xl font-semibold inline-flex items-center">
+            Maxime <GoVerified className="ml-1.5" />
+          </span>
+          <span className="text-lg relative -top-1">@maxess</span>
+          <div className="inline-flex items-center py-4 gap-x-2 font-semibold">
+            <span className="inline-flex w-full items-center gap-x-2.5">
+              <span className="bg-accent rounded-full w-10 h-10 inline-flex justify-center items-center">
+                <CgSearch size="1.4em" />
+              </span>
+              Recherche un groupe
+            </span>
+          </div>
+          <div className="mt-2 flex gap-x-16">
+            <div className="space-y-1.5">
+              <span className="text-sm">À propos :</span>
+              <ul className="text-base space-y-1 flex flex-col">
+                <li className="inline-flex items-center gap-x-2">
+                  <HiOutlineLocationMarker size={"1.2em"} /> Toulouse (31)
+                </li>
+
+                <li className="inline-flex items-center gap-x-2">
+                  <IoMdTime size={"1.2em"} /> Connecté(e) il y a 1h
+                </li>
+                <li className="inline-flex items-center gap-x-2">
+                  <AiOutlineUser size={"1.1em"} /> 25 ans
+                </li>
+              </ul>
+            </div>
+            <div className="space-y-1.5">
+              <span className="text-sm">L'essentiel :</span>
+              <ul className="text-base space-y-1 flex flex-col">
+                <li className="inline-flex items-center gap-x-2">
+                  <RiUserSearchLine size={"1.2em"} /> Musicien
+                </li>
+                <li className="inline-flex items-center gap-x-2">
+                  <TbCrown size={"1.2em"} />
+                  Guitare, Piano
+                </li>
+                <li className="inline-flex items-center gap-x-2">
+                  <CiMusicNote1 size={"1.2em"} />
+                  Rock, Jazz, Hip-Hop
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="space-y-3 mt-10">
         <div className="w-full flex gap-4">
           <div className="w-full">
-            <div className="space-y-8 bg-muted-background p-6">
+            <div className="space-y-8 bg-background border border-secondary rounded-lg p-6">
               <div className="space-y-3">
                 <div className="text-xl">
                   <h2 className="font-semibold">Instruments pratiqués</h2>
