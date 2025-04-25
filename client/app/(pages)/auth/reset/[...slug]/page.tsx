9b3a7bcd1a0226d70dc725c342864ef730008c02
maxess3 <maxschell31@gmail.com>
2025-04-25 19:14:39 +0200

    update: env

--- client/app/(pages)/auth/reset/[...slug]/page.tsx
+++ client/app/(pages)/auth/reset/[...slug]/page.tsx
@@ -1,46 +1,45 @@
 import ResetPasswordForm from "@/components/general/_partials/form/ResetPasswordForm";
 import { redirect } from "next/navigation";
-import { BASE_URL } from "@/lib/constants";
 
 export default async function Reset({
-  params,
+	params,
 }: {
-  params: Promise<{ slug: string }>;
+	params: Promise<{ slug: string }>;
 }) {
-  const slug = (await params).slug;
-  const id = slug[0];
-  const token = slug[1];
+	const slug = (await params).slug;
+	const id = slug[0];
+	const token = slug[1];
 
-  if (!id || !token) {
-    redirect("/");
-  }
+	if (!id || !token) {
+		redirect("/");
+	}
 
-  const validateToken = async () => {
-    try {
-      const res = await fetch(
-        `${BASE_URL}/auth/validate-reset-token?token=${token}`,
-        {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-          cache: "no-store",
-        }
-      );
+	const validateToken = async () => {
+		try {
+			const res = await fetch(
+				`${process.env.NEXT_PUBLIC_API_URL}/auth/validate-reset-token?token=${token}`,
+				{
+					method: "GET",
+					headers: { "Content-Type": "application/json" },
+					cache: "no-store",
+				}
+			);
 
-      if (!res.ok) {
-        throw new Error("Invalid token");
-      }
-      const data = await res.json();
-      return data.valid;
-    } catch (error) {
-      if (error) return false;
-    }
-  };
+			if (!res.ok) {
+				throw new Error("Invalid token");
+			}
+			const data = await res.json();
+			return data.valid;
+		} catch (error) {
+			if (error) return false;
+		}
+	};
 
-  const isValid = await validateToken();
+	const isValid = await validateToken();
 
-  if (!isValid) {
-    redirect("/");
-  }
+	if (!isValid) {
+		redirect("/");
+	}
 
-  return <ResetPasswordForm userId={id} token={token} />;
+	return <ResetPasswordForm userId={id} token={token} />;
 }
