3e913b11f7a1261cfb5c0e76ff31e568de212284
maxess3 <maxschell31@gmail.com>
2025-04-26 12:27:00 +0200

    fix: focus in dropdown menu

--- client/components/general/_partials/button/Radio.tsx
+++ client/components/general/_partials/button/Radio.tsx
@@ -2,27 +2,27 @@ import { Label } from "@/components/ui/label";
 import { RadioGroupItem } from "@/components/ui/radio-group";
 
 export function Radio({
-  className,
-  title,
-  id,
-  value,
+	className,
+	title,
+	id,
+	value,
 }: {
-  className?: string;
-  title: string;
-  id: string;
-  value: string;
+	className?: string;
+	title: string;
+	id: string;
+	value: string;
 }) {
-  return (
-    <>
-      <Label
-        htmlFor={id}
-        className={`border cursor-pointer rounded-md px-3 h-9 flex justify-center flex-col gap-2 [&:has(:checked)]:bg-transparent dark:[&:has(:checked)]:bg-transparent [&:has(:checked)]:border-primary ${className}`}
-      >
-        <div className="flex items-center gap-2">
-          <RadioGroupItem id={id} value={value} />
-          {title}
-        </div>
-      </Label>
-    </>
-  );
+	return (
+		<>
+			<Label
+				htmlFor={id}
+				className={`border cursor-pointer rounded-md px-3 h-9 flex justify-center flex-col gap-2 [&:has(:checked)]:bg-transparent dark:[&:has(:checked)]:bg-transparent [&:has(:checked)]:border-foreground ${className}`}
+			>
+				<div className="flex items-center gap-2">
+					<RadioGroupItem id={id} value={value} />
+					{title}
+				</div>
+			</Label>
+		</>
+	);
 }
--- client/components/modal/Modal.tsx
+++ client/components/modal/Modal.tsx
@@ -2,103 +2,103 @@
 
 import { z } from "zod";
 import {
-  useForm,
-  FormProvider,
-  SubmitHandler,
-  FieldValues,
-  DefaultValues,
+	useForm,
+	FormProvider,
+	SubmitHandler,
+	FieldValues,
+	DefaultValues,
 } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import {
-  Dialog,
-  DialogOverlay,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogFooter,
+	Dialog,
+	DialogOverlay,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogDescription,
+	DialogFooter,
 } from "../ui/dialog";
 import { Button } from "../ui/button";
 import { Loader2 } from "lucide-react";
 
 interface ModalProps<T extends FieldValues> {
-  children: React.ReactNode;
-  route?: string;
-  title: string;
-  onSubmit: (data: T) => Promise<void>;
-  formSchema: z.ZodType<T>;
-  defaultValues?: DefaultValues<T>;
-  isSubmitting?: boolean;
-  open?: boolean;
+	children: React.ReactNode;
+	route?: string;
+	title: string;
+	onSubmit: (data: T) => Promise<void>;
+	formSchema: z.ZodType<T>;
+	defaultValues?: DefaultValues<T>;
+	isSubmitting?: boolean;
+	open?: boolean;
 }
 
 export function Modal<T extends FieldValues>({
-  children,
-  route,
-  title,
-  onSubmit,
-  formSchema,
-  defaultValues,
-  isSubmitting = false,
-  open = true,
+	children,
+	route,
+	title,
+	onSubmit,
+	formSchema,
+	defaultValues,
+	isSubmitting = false,
+	open = true,
 }: ModalProps<T>) {
-  const router = useRouter();
-  const handleOpenChange = () => {
-    if (window.history.length > 2) {
-      router.back();
-    } else if (route) {
-      router.push(route);
-    }
-  };
+	const router = useRouter();
+	const handleOpenChange = () => {
+		if (window.history.length > 2) {
+			router.back();
+		} else if (route) {
+			router.push(route);
+		}
+	};
 
-  const methods = useForm<T>({
-    mode: "onChange",
-    resolver: zodResolver(formSchema),
-    defaultValues,
-  });
+	const methods = useForm<T>({
+		mode: "onChange",
+		resolver: zodResolver(formSchema),
+		defaultValues,
+	});
 
-  const formSubmit: SubmitHandler<T> = async (data) => {
-    console.log(data);
-    await onSubmit(data);
-  };
+	const formSubmit: SubmitHandler<T> = async (data) => {
+		console.log(data);
+		await onSubmit(data);
+	};
 
-  return (
-    <div>
-      <Dialog defaultOpen={open} open={open} onOpenChange={handleOpenChange}>
-        <DialogOverlay>
-          <DialogContent className="md:max-w-2xl sm:max-w-2xl max-w-full sm:max-h-[85%] max-h-full p-0">
-            <DialogHeader>
-              <DialogTitle>
-                <DialogDescription className="text-xl text-foreground">
-                  {title}
-                </DialogDescription>
-              </DialogTitle>
-            </DialogHeader>
-            <div className="overflow-y-auto px-6 pt-6 pb-9 space-y-6">
-              <FormProvider {...methods}>
-                <form
-                  id="modalForm"
-                  onSubmit={methods.handleSubmit(formSubmit)}
-                >
-                  {children}
-                </form>
-              </FormProvider>
-            </div>
-            <DialogFooter>
-              <Button
-                disabled={isSubmitting}
-                type="submit"
-                variant="primary"
-                form="modalForm"
-              >
-                Enregistrer
-                {isSubmitting ? <Loader2 className="animate-spin" /> : ""}
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </DialogOverlay>
-      </Dialog>
-    </div>
-  );
+	return (
+		<div>
+			<Dialog defaultOpen={open} open={open} onOpenChange={handleOpenChange}>
+				<DialogOverlay>
+					<DialogContent className="md:max-w-2xl sm:max-w-2xl max-w-full sm:max-h-[85%] max-h-full p-0">
+						<DialogHeader>
+							<DialogTitle>
+								<DialogDescription className="text-xl text-foreground">
+									{title}
+								</DialogDescription>
+							</DialogTitle>
+						</DialogHeader>
+						<div className="overflow-y-auto px-6 pt-6 pb-9 space-y-6">
+							<FormProvider {...methods}>
+								<form
+									id="modalForm"
+									onSubmit={methods.handleSubmit(formSubmit)}
+								>
+									{children}
+								</form>
+							</FormProvider>
+						</div>
+						<DialogFooter>
+							<Button
+								disabled={isSubmitting}
+								type="submit"
+								variant="default"
+								form="modalForm"
+							>
+								Enregistrer
+								{isSubmitting ? <Loader2 className="animate-spin" /> : ""}
+							</Button>
+						</DialogFooter>
+					</DialogContent>
+				</DialogOverlay>
+			</Dialog>
+		</div>
+	);
 }
