bb78a24f84bedbbdb7cb6c9a057eb0c90e48a9b4
maxess3 <maxschell31@gmail.com>
2025-05-17 12:25:00 +0200

    update: profile

--- client/components/pages/profile/button/EditProfileButton.tsx
+++ client/components/pages/profile/button/EditProfileButton.tsx
@@ -1,17 +1,15 @@
-import { Pencil } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { saveLastFocusedElement } from "@/utils/utils";
 
 export const EditProfileButton = ({ username }: { username: string }) => {
-  return (
-    <Link
-      href={`/${username}/edit/profile/general`}
-      onClick={saveLastFocusedElement}
-      className={buttonVariants({ variant: "outline" })}
-    >
-      <Pencil className="mr-1" style={{ width: "1.15em", height: "1.15em" }} />
-      Modifier mon profil
-    </Link>
-  );
+	return (
+		<Link
+			href={`/${username}/edit/profile/general`}
+			onClick={saveLastFocusedElement}
+			className={buttonVariants({ variant: "outline" })}
+		>
+			Modifier mon profil
+		</Link>
+	);
 };
--- client/components/pages/profile/header/ProfilePicture.tsx
+++ client/components/pages/profile/header/ProfilePicture.tsx
@@ -5,51 +5,57 @@ import { EditProfilePictureModal } from "@/components/modal/EditProfilePictureMo
 import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { PiUser } from "react-icons/pi";
-
+import { Button } from "@/components/ui/button";
+import { PiMusicNotesPlus } from "react-icons/pi";
 export const ProfilePicture = ({
-  isOwner,
-  src,
-  alt,
+	isOwner,
+	src,
+	alt,
 }: {
-  isOwner: boolean;
-  src: string;
-  alt: string;
+	isOwner: boolean;
+	src: string;
+	alt: string;
 }) => {
-  const { data: session } = useSession();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const defaultImage = "/profile_large.jpeg";
+	const { data: session } = useSession();
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const imageURL = process.env.NEXT_PUBLIC_R2_URL + "/" + src;
+	const imageURL = process.env.NEXT_PUBLIC_R2_URL + "/" + src;
 
-  const IMG_PROFILE = (
-    <Avatar className="w-48 h-48">
-      <AvatarImage src={imageURL} alt={alt} />
-      <AvatarFallback>
-        <PiUser size={58} />
-      </AvatarFallback>
-    </Avatar>
-  );
+	const IMG_PROFILE = (
+		<Avatar className="w-48 h-48">
+			<AvatarImage className="hover:opacity-90" src={imageURL} alt={alt} />
+			<AvatarFallback>
+				<PiUser size={58} />
+			</AvatarFallback>
+		</Avatar>
+	);
 
-  return (
-    <>
-      <div className="w-52 h-52 flex rounded-full">
-        {isOwner && session?.user?.username ? (
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className="rounded-full relative overflow-hidden group hover:opacity-90"
-          >
-            {IMG_PROFILE}
-          </button>
-        ) : (
-          IMG_PROFILE
-        )}
-      </div>
-      {isModalOpen && (
-        <EditProfilePictureModal
-          onClose={() => setIsModalOpen(false)}
-          open={isModalOpen}
-        />
-      )}
-    </>
-  );
+	return (
+		<div className="relative">
+			<div className="w-52 h-52 flex rounded-full relative">
+				{isOwner && session?.user?.username ? (
+					<button
+						onClick={() => setIsModalOpen(true)}
+						className="rounded-full relative overflow-hidden group"
+					>
+						{IMG_PROFILE}
+					</button>
+				) : (
+					IMG_PROFILE
+				)}
+				<Button
+					variant={"outline"}
+					className="bg-background absolute bottom-2 right-8 h-12 w-12 rounded-full"
+				>
+					<PiMusicNotesPlus style={{ width: "1.2em", height: "1.2em" }} />
+				</Button>
+			</div>
+			{isModalOpen && (
+				<EditProfilePictureModal
+					onClose={() => setIsModalOpen(false)}
+					open={isModalOpen}
+				/>
+			)}
+		</div>
+	);
 };
