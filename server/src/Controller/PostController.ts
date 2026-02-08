cc459e0a86e152a7c230e624d1c2ba21d95e2720
maxess3 <maxschell31@gmail.com>
2026-02-08 12:14:49 +0100

    feat: implement authentication and user profile management controllers, including signup, login, password reset, and profile updates

---
+++ server/src/Controller/PostController.ts
@@ -0,0 +1,19 @@
+import prisma from "../db/db.config";
+import { Request, Response } from "express";
+
+export const createPost = async (req: Request, res: Response) => {
+  const { user_id, country, department, city, title, content } = req.body;
+
+  const newPost = await prisma.post.create({
+    data: {
+      user_id: String(user_id),
+      country,
+      department,
+      city,
+      title,
+      content,
+    },
+  });
+
+  return res.status(200).json({ data: newPost, message: "Post created." });
+};
